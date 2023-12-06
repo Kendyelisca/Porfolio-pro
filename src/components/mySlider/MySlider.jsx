5763da3689e2ac2bf09bbc2f9a4ab4f31743d5f4
Kendy <kendyelisca5@gmail.com>
2023-12-06 17:36:46 -0500

    optimized look

--- src/components/mySlider/MySlider.jsx
+++ src/components/mySlider/MySlider.jsx
@@ -1,241 +1,119 @@
 import React from 'react';
-import Slider from 'react-slick';
+
 import './myslider.css';
 import { AiFillEye } from 'react-icons/ai';
 import { FaGithub } from 'react-icons/fa';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import { useTranslation } from 'react-i18next';
 import { t } from 'i18next';
-function Arrow(props) {
-  const { className, style, onClick } = props;
-
-  return (
-    <div
-      className={className}
-      style={{ ...style, display: 'block', background: 'black' }}
-      onClick={onClick}
-    />
-  );
-}
 
 const MySlider = () => {
-  var settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    nextArrow: <Arrow />,
-    prevArrow: <Arrow />,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true,
-        },
-      },
-
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 2,
-        },
-      },
-    ],
-  };
   const { t } = useTranslation();
 
   return (
     <div id="projects" className="m-10 ">
       <h2 className="text-center text-4xl pb-5 font-bold">{t('Projects')}</h2>
-      <Slider {...settings} className="custom-slider">
-        <div className="project">
-          <img src={'./CartToDoor-logo.png'} alt="rick y morty" />
-          <p>
-            <strong>CartToDOORMARKET APP</strong>
-          </p>
-          <div class="overlay">
-            <a
-              class="icon"
-              href="https://carttodoormarket-com.vercel.app/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <AiFillEye />
-            </a>
-            <a
-              class="icon"
-              href="https://github.com/Kendyelisca/carttodoor_app"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FaGithub />
-            </a>
+      <div class="projects-container">
+        {' '}
+        <article class="card-project">
+          <div class="card__img">
+            <img src="./news-p.png" alt="" />
+            <img src="./news-d.png" alt="" />
           </div>
-        </div>
-        <div className="project">
-          <img src={'./newsflow.png'} alt="rick y morty" />
-          <p>
-            <strong>NewsFlow APP</strong>
-          </p>
-          <div class="overlay">
-            <a
-              className="icon"
-              href="https://news-flow-two.vercel.app/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <AiFillEye />
-            </a>
-            <a
-              class="icon"
-              href="https://github.com/Kendyelisca/NewsFlow"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FaGithub />
-            </a>
-          </div>
-        </div>
-        <div className="project">
-          <img src={'./mortyApp.webp'} alt="rick y morty" />
-          <p>
-            <strong>Rick y Morty App</strong>
-          </p>
-          <div class="overlay">
-            <a
-              class="icon"
-              href="https://strong-mooncake-11c81e.netlify.app/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <AiFillEye />
-            </a>
-            <a
-              class="icon"
-              href="https://github.com/Kendyelisca/Rick-y-Morty"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FaGithub />
-            </a>
-          </div>
-        </div>
+          <div class="card__content--project">
+            <h2 className="text-2xl md:text-3xl lg:text-4xl">NewsFlow</h2>
+            <p>
+              NewsFlow creates a user-friendly platform for accessing news, sharing
+              stories, and engaging with a dynamic community. Prioritizing user
+              convenience and content personalization, the system integrates the Gnews API
+              for a constant influx of fresh and diverse news content.
+            </p>
+            <span>
+              Technologies:Next.js, Tailwind CSS, Node.js, Express.js, JWT, PostgreSQL,
+              NodeCron, Nodemailer, Gnews API, Socket.io.
+            </span>
 
-        <div className="project">
-          <img src={'./weather.webp'} alt="rick y morty" />
-          <p>
-            <strong>Weather App</strong>
-          </p>
-          <div class="overlay">
-            <a
-              class="icon"
-              href="https://relaxed-cranachan-bfb222.netlify.app/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <AiFillEye />
-            </a>
-            <a
-              className="icon"
-              href="https://github.com/Kendyelisca/clima"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FaGithub />
-            </a>
+            <div class="btn__projects">
+              <a href="https://news-flow-two.vercel.app/">
+                <AiFillEye />
+              </a>
+              <a href="https://github.com/Kendyelisca/NewsFlow">
+                <FaGithub />
+              </a>
+            </div>
           </div>
-        </div>
-        <div className="project">
-          <img src={'./userAdmin.avif'} alt="rick y morty" />
-          <p>
-            <strong>CRUD App</strong>
-          </p>
-          <div class="overlay">
-            <a
-              className="icon"
-              href="https://poetic-palmier-61ea7e.netlify.app/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <AiFillEye />
-            </a>
-            <a
-              className="icon"
-              href="https://github.com/Kendyelisca/my-crud"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FaGithub />
-            </a>
+        </article>
+        <article class="card-project">
+          <div class="card__img">
+            <img src="./poke-p.png" alt="" />
+            <img src="./poke-d.png" alt="" />
           </div>
-        </div>
-        <div className="project">
-          <img src={'./random.png'} alt="rick y morty" />
-          <p>
-            <strong>Random Quote App</strong>
-          </p>
-          <div class="overlay">
-            <a
-              class="icon"
-              href="https://delightful-dragon-bc9c02.netlify.app/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <AiFillEye />
-            </a>
-            <a
-              class="icon"
-              href="https://github.com/Kendyelisca/Quote-Generator"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FaGithub />
-            </a>
-          </div>
-        </div>
+          <div class="card__content--project">
+            <h2 className="text-2xl md:text-3xl lg:text-4xl">Rick and Morty Wiki</h2>
+            <p>
+              Explore the vast universe of Rick and Morty with this comprehensive wiki.
+              Dive into the multiverse, discover intriguing characters, and unravel the
+              mind-bending plots that make this animated series a fan favorite.
+            </p>
+            <span>Technologies: ReactJs, React Hooks, CSS, Axios</span>
 
-        <div className="project">
-          <img src={'./ecommerce.jpg'} alt="rick y morty" />
-          <p>
-            <strong>E-commerce App</strong>
-          </p>
-          <div class="overlay">
-            <a
-              className="icon"
-              href="https://kendy-comm.vercel.app/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <AiFillEye />
-            </a>
-            <a
-              class="icon"
-              href="https://github.com/Kendyelisca/kendy-comm"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FaGithub />
-            </a>
+            <div class="btn__projects">
+              <a href="https://strong-mooncake-11c81e.netlify.app/">
+                <AiFillEye />
+              </a>
+              <a href="https://github.com/Kendyelisca/Rick-y-Morty">
+                <FaGithub />
+              </a>
+            </div>
           </div>
-        </div>
-      </Slider>
+        </article>
+        <article class="card-project">
+          <div class="card__img">
+            <img src="./quote-p.png" alt="" />
+            <img src="./quote-d.png" alt="" />
+          </div>
+          <div class="card__content--project">
+            <h2 className="text-2xl md:text-3xl lg:text-4xl">Quote Generator</h2>
+            <p>
+              Immerse yourself in a world of wisdom and inspiration with this React-based
+              Quote Generator. Experience the joy of discovering thought-provoking and
+              motivational quotes at the click of a button. Powered by React, this web
+              application seamlessly delivers a daily dose of positivity to uplift and
+              inspire users.
+            </p>
+            <span>Technologies: ReactJs, React Hooks, CSS, Axios</span>
+            <div class="btn__projects">
+              <a href="https://delightful-dragon-bc9c02.netlify.app/">
+                <AiFillEye />
+              </a>
+              <a href="https://github.com/Kendyelisca/Quote-Generator">
+                <FaGithub />
+              </a>
+            </div>
+          </div>
+        </article>
+        <article class="card-project">
+          <div class="card__img">
+            <img src="./weather-p.png" alt="" />
+            <img src="./weather-d.png" alt="" />
+          </div>
+          <div class="card__content--project">
+            <h2 className="text-2xl md:text-3xl lg:text-4xl">Weather App</h2>
+            <p>
+              This application gives you the temperature in real time according to the
+              location of the browser. As well as the city you enter.
+            </p>
+            <span>Technologies: ReactJs, React Hooks, CSS, Axios</span>
+            <div class="btn__projects">
+              <a href="https://relaxed-cranachan-bfb222.netlify.app/">
+                <AiFillEye />
+              </a>
+              <a href="https://github.com/Kendyelisca/clima">
+                <FaGithub />
+              </a>
+            </div>
+          </div>
+        </article>
+      </div>
     </div>
   );
 };
